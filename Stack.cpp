86c0466c9bd232e45ca6dccaf352fe10151c2b3c
thaqiulislamkafi <thaqiulislamkafi@tuta.io>
2024-08-17 01:42:47 +0600

    Create Stack.cpp

---
+++ Stack.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std ;
+
+   print(stack<int>st){
+
+       while(!st.empty()){
+        cout << st.top()<< " ";
+        st.pop() ;
+       }
+       cout << endl ;
+//        for(auto x : li) cout << x << " " ;
+//    cout << endl ;
+   }
+
+int main (){
+
+    stack<int>st  ;
+
+    /// insert element in stack
+
+    st.push(6); st.push(7);
+    st.push(8); st.push(9);   print(st);
+
+    /// Delete element in stack
+
+//    st.pop(); st.pop() ;  print(st);
+
+    /// Is stack empty ?
+
+//    if(!st.empty()) cout << "Yes" << " " ;
+
+    /// stack size
+
+//    cout << st.size()<< " " ;
+
+
+}
